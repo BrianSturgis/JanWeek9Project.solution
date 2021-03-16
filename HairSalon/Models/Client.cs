88867389f9024161ebf9bba61f54e4e8cce009a7
Brian Sturgis <sturujisu@gmail.com>
2021-03-15 20:46:05 -0700

    update project after massive refactor

--- HairSalon/Models/Client.cs
+++ HairSalon/Models/Client.cs
@@ -1,11 +1,8 @@
-namespace HairSalon.Models
-{
+namespace HairSalon.Models{
   public class Client
   {
     public int ClientId { get; set; }
     public string Name { get; set; }
-    public string Address { get; set; }
-    public int PhoneNumber { get; set; }
     public int StylistId { get; set; }
     public virtual Stylist Stylist { get; set; }
   }
